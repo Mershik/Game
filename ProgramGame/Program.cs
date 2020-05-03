70bbfd38c60da3e18f5ae508099f58db89a53235
mershik <eugene_miroshnichenko@mail.ru>
2020-05-03 20:51:34 +0300

    Добавлен критический удар и условия победы

--- ProgramGame/Program.cs
+++ ProgramGame/Program.cs
@@ -9,26 +9,42 @@ namespace Fight
 {
     class Program
     {
-        static int Fight(int healthHero)
+        static int Fight(int healthHero, int attackH)
         {
             int healthMonster = 100;
-            int attackH;
             int attackM;
+
             Random attackHero = new Random();
             Random attackMonster = new Random();
+            Random critAttackHero = new Random();
 
             while (healthHero > 0)
             {
-                Console.WriteLine();
-                attackH = attackHero.Next(20, 35);
-                healthMonster -= attackH;
+                if (critAttackHero.Next(1, 5) == 1)
+                {
+                    Console.WriteLine();
+                    attackH = attackHero.Next(20, attackH);
+                    healthMonster -= attackH*2;
 
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"Герой бъет с силой {attackH}.");               
-                Console.ResetColor();
-                Console.WriteLine($"У монстра осталось { healthMonster} HP"); 
-                Thread.Sleep(1000);
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine($"Герой наности критический удар {attackH*2}!");               
+                    Console.ResetColor();
+                    Console.WriteLine($"У монстра осталось { healthMonster} HP"); 
+                    Thread.Sleep(1000);
+                }
+                else
+                {
+                    Console.WriteLine();
+                    attackH = attackHero.Next(20, attackH);
+                    healthMonster -= attackH;
 
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Герой бъет с силой {attackH}.");
+                    Console.ResetColor();
+                    Console.WriteLine($"У монстра осталось { healthMonster} HP");
+                    Thread.Sleep(1000);
+                }
+              
                 if (healthMonster <= 0)
                 {
                     Console.WriteLine();
@@ -62,7 +78,7 @@ namespace Fight
             if (healthHero > 0)
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Победа! Вы получили кучу опыта и 50 HP!");
+                Console.WriteLine("Монстр мертв и его душа излечивает вас на 50 HP!");
                 Console.ResetColor();
                 Console.WriteLine("Ваше ХП " + (healthHero + 50));
             }
@@ -78,18 +94,29 @@ namespace Fight
 
         static void Main(string[] args)
         {
-            Console.WriteLine("На вас напали!");
+            Console.WriteLine(@"Вы спускаетесь в подземелье и видите, 
+ как на вас прыгает свирепый монстр");
 
             int healthHero = 100;
+            int attackH = 30;
             int count = 0;
 
             while (healthHero > 0)
             {
-                healthHero = Fight(healthHero);
+                healthHero = Fight(healthHero, attackH);
                 finalFight(healthHero);
                 count++;
+                healthHero += 50;
+                if (count == 4)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Победа! Вы уничтожили {count - 1} монстров! Подземелье в безопасности");                   
+                    Console.ResetColor();
+                    return;
+                }
             }
             Console.WriteLine($"Вы уничтожили {count - 1} монстров");
+            
             Console.ReadKey();
 
         }
