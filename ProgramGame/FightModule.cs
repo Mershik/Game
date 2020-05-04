fc0898d88f869d43fa8de9a50796fa6a8f85833d
mershik <eugene_miroshnichenko@mail.ru>
2020-05-04 18:11:42 +0300

    Создан отдельный класс FightModule

---
+++ ProgramGame/FightModule.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading;
+
+namespace ProgramGame
+{
+    public class FightModule 
+    {
+
+        public int Fight(int healthHero,
+                         int healthMonster)
+        {
+            int attackM;
+            int attackH;
+
+            Random attackHero = new Random();
+            Random attackMonster = new Random();
+            Random critAttackHero = new Random();
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Вы достаете оружие и готовитесь к битве");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("HP героя = " + healthHero);
+            Console.WriteLine("HP монстра = " + healthMonster);
+            Thread.Sleep(3000);
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("В Атаку!!!");
+            Console.ResetColor();
+
+            while (healthHero > 0)
+            {
+                if (critAttackHero.Next(1, 5) == 1)
+                {
+                    Console.WriteLine();
+                    attackH = attackHero.Next(10, 25);
+                    healthMonster -= attackH * 2;
+
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine($"Герой наносит критический удар {attackH * 2}!");
+                    Console.ResetColor();
+                    Console.WriteLine($"У монстра осталось { healthMonster} HP");
+                    Thread.Sleep(500);
+                }
+                else
+                {
+                    Console.WriteLine();
+                    attackH = attackHero.Next(10, 25);
+                    healthMonster -= attackH;
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Герой бъет с силой {attackH}.");
+                    Console.ResetColor();
+                    Console.WriteLine($"У монстра осталось { healthMonster} HP");
+                    Thread.Sleep(500);
+                }
+
+                if (healthMonster <= 0)
+                {
+                    Console.WriteLine();
+                    return healthHero;
+                }
+
+                attackM = attackMonster.Next(15, 25);
+                healthHero -= attackM;
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Монстр кусает зубами на {attackM}");
+                Console.ResetColor();
+                Console.WriteLine($"У героя осталось {healthHero} HP");
+                Thread.Sleep(1000);
+                Console.WriteLine();
+
+                if (healthHero <= 0)
+                {
+                    return healthHero;
+                }
+                else if (healthMonster > 0 && healthHero > 0)
+                {
+                    continue;
+                }
+            }
+            return healthHero;
+        }
+    }
+}
--- ProgramGame/Program.cs
+++ ProgramGame/Program.cs
@@ -1,89 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
+using ProgramGame;
 
 namespace Fight
 {
     class Program
     {
-        static int Fight(int healthHero, int healthMonster)
-        {           
-            int attackM;
-            int attackH;
-
-            Random attackHero = new Random();
-            Random attackMonster = new Random();
-            Random critAttackHero = new Random();
-
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("Вы достаете оружие и готовитесь к битве");
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
-            Console.WriteLine("HP героя = " + healthHero);
-            Console.WriteLine("HP монстра = " + healthMonster);
-            Thread.Sleep(3000);
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("В Атаку!!!");
-            Console.ResetColor();
-
-            while (healthHero > 0)
-            {
-                if (critAttackHero.Next(1, 5) == 1)
-                {
-                    Console.WriteLine();
-                    attackH = attackHero.Next(10, 25);
-                    healthMonster -= attackH*2;
-
-                    Console.ForegroundColor = ConsoleColor.DarkYellow;
-                    Console.WriteLine($"Герой наносит критический удар {attackH*2}!");               
-                    Console.ResetColor();
-                    Console.WriteLine($"У монстра осталось { healthMonster} HP"); 
-                    Thread.Sleep(500);
-                }
-                else
-                {
-                    Console.WriteLine();
-                    attackH = attackHero.Next(10, 25);
-                    healthMonster -= attackH;
-
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"Герой бъет с силой {attackH}.");
-                    Console.ResetColor();
-                    Console.WriteLine($"У монстра осталось { healthMonster} HP");
-                    Thread.Sleep(500);
-                }
-              
-                if (healthMonster <= 0)
-                {
-                    Console.WriteLine();
-                    return healthHero;
-                }
-
-                attackM = attackMonster.Next(15, 25);
-                healthHero -= attackM;
-
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Монстр кусает зубами на {attackM}");
-                Console.ResetColor();
-                Console.WriteLine($"У героя осталось {healthHero} HP");
-                Thread.Sleep(1000);
-                Console.WriteLine();
-
-                if (healthHero <= 0)
-                {
-                    return healthHero;
-                }
-                else if (healthMonster > 0 && healthHero > 0)
-                {
-                    continue;
-                }
-            }
-            return healthHero;
-        }
-
         static int finalFight(int healthHero)
         {
             if (healthHero > 0)
@@ -99,7 +21,6 @@ namespace Fight
                 Console.WriteLine("Герой храбро погиб");
                 Console.ResetColor();
             }
-
             return healthHero;
         }
 
@@ -114,9 +35,11 @@ namespace Fight
 Какую выберете 1 или 2?");
             Console.ResetColor();
 
-            String direction = Console.ReadLine();
+            FightModule fightModule = new FightModule();
+            
 
-            switch (direction)
+
+            switch (Console.ReadLine())
             {
                 case "1":
                     Console.WriteLine();                   
@@ -124,7 +47,7 @@ namespace Fight
                     Console.WriteLine("Внезапно вы видите приближающегося огромного монстра, который вступает с вами в бой");
                     Thread.Sleep(5000);
 
-                    healthHero = Fight(healthHero, 150);
+                    healthHero = fightModule.Fight(healthHero, 150);
                     finalFight(healthHero);
                     if (healthHero > 0)
                     {
@@ -160,7 +83,7 @@ namespace Fight
                             Console.WriteLine();
                             Console.WriteLine("В сундуке вы находите яйцо монстра, которое вылупляется и атакует");
                             Thread.Sleep(5000);
-                            healthHero = Fight(healthHero, 70);
+                            healthHero = fightModule.Fight(healthHero, 70);
                             finalFight(healthHero);
                             if (healthHero > 0)
                             {
@@ -186,13 +109,20 @@ namespace Fight
                     for (int i = 1; i < 4; i++)
                     {
                         Console.WriteLine($"{i} монстр нападает");
-                        healthHero = Fight(healthHero, 150);
+                        healthHero = fightModule.Fight(healthHero, 500);
                         finalFight(healthHero);
                         Console.WriteLine();
                         Console.WriteLine($"Ты уничтожил {i} монстра");
                         Console.WriteLine();
                         Thread.Sleep(3000);
-                        count++;
+                        
+                        if (healthHero > 0)
+                        {
+                            healthHero += 50;
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.WriteLine("Вы истребили потомство, которое не давало жить этой долине.");
+                            count++;
+                        }
                     }
                     Console.WriteLine("Комната чиста");
                     Thread.Sleep(5000);
